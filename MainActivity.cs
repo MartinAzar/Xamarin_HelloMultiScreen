b749fa0b8d5a9ad9297ff0384ae4282d76ea7b41
Martin Azar <MartinPabloAzar@gmail.com>
2017-06-12 11:42:37 +1000

    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,33 @@
+using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloMultiScreen
+{
+	[Activity (Label = "HelloMultiScreen", MainLauncher = true)]
+	public class FirstActivity : Activity
+	{
+//		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			//Use UI created in Main.axml
+			SetContentView (Resource.Layout.Main);
+
+			var showSecond = FindViewById<Button> (Resource.Id.showSecond);
+			showSecond.Click += (sender, e) => {
+				var second = new Intent(this, typeof(SecondActivity));
+				second.PutExtra("FirstData", "Data from FirstActivity");
+				StartActivity(second);
+			};
+		}
+
+	}
+}
+
+
